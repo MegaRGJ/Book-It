a7e0c822b0463750ed4e5607de0cb8ea6d2f9937
Adam <ad1996@hotmail.co.uk>
2018-07-26 16:09:07 +0100

    Fixed permission system, added data entry options

--- Libraries/BookItDependancies/BookItDependancies/Server.cs
+++ Libraries/BookItDependancies/BookItDependancies/Server.cs
@@ -335,7 +335,6 @@ namespace BookItDependancies
         //3 - Business Admin - View and edit all employee data
         //4 - Owner    - View and edit all employee and business data
 
-        #region Main Commands
         /// <summary>
         /// Log in routine, used to verify identity of user
         /// </summary>
@@ -390,6 +389,7 @@ namespace BookItDependancies
         /// <returns></returns>
         public string AddUser(string Name, string Address, string Postcode, string Email, string Phone, string Username, string Password, int PermissionLevel)
         {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
             if (clientPermissionLevel < 2)
                 return null;
             //First encrypt user's new password
@@ -404,6 +404,104 @@ namespace BookItDependancies
             List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
             return ServerCommunication.NewTableEntry("Users", encryptedData);       //Add data to table
         }
+        /// <summary>
+        /// Create new Business to table
+        /// </summary>
+        /// <param name="Name">Full name of user</param>
+        /// <param name="Address">Number and Street name</param>
+        /// <param name="Postcode">User's postcode</param>
+        /// <param name="Phone">Contact phone number</param>
+        /// <param name="Email">Email address</param>
+        /// <param name="Description">Description of the business (Should include what the company offers, can include http links</param>
+        /// <param name="active">Is the business active?</param>
+        /// <param name="sharedBookings">Does the business have bookings split between all employees?</param>
+        /// <returns></returns>
+        public string AddBusiness(string Name, string Address, string Postcode, string phone, string email, string Description, bool active, bool sharedBookings)
+        {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
+            if (clientPermissionLevel != 3) return null; //Only admins can add new business accounts
+            List<string> newData = new List<string>() { Name, Address, Postcode, phone, email, Description, active.ToString(), sharedBookings.ToString() };
+            List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
+            return ServerCommunication.NewTableEntry("Businesses", encryptedData);  //Add data to table
+        }
+        /// <summary>
+        /// Create a new Employee entry to database
+        /// </summary>
+        /// <param name="businessID">Business ID of employee</param>
+        /// <param name="userID">User ID of employee</param>
+        /// <param name="permissionLevel">Permission level of new employee</param>
+        /// <param name="availability">Availability of employee (see documentation for details)</param>
+        /// <param name="ammendments">Ammendments to availability (see documentation for details) </param>
+        /// <returns></returns>
+        public string AddEmployee(int businessID, int userID, string permissionLevel, List<string> availability, List<string> ammendments)
+        {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
+            if (clientPermissionLevel != 3 || (businessID == clientBusinessID && clientEmployeePermissionLvl == 4)) return null; //Only admins and business owners can create employee accounts
+            List<string> newData = new List<string>() { businessID.ToString(), userID.ToString(), permissionLevel, FormatAvailability(availability), FormatAmmendments(ammendments) };
+            List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
+            return ServerCommunication.NewTableEntry("Employees", encryptedData);   //Add data to table
+        }
+        /// <summary>
+        /// Create a new booking in the database
+        /// </summary>
+        /// <param name="employeeID">ID of employee booked with</param>
+        /// <param name="userID">User ID being booked in</param>
+        /// <param name="bookingDateTime">Date Time of the booking</param>
+        /// <param name="duration">Duration (in minutes) of the booking</param>
+        /// <param name="description">Description (if required)</param>
+        /// <param name="cancellation">Cancellation reason (booking is conisdered cancelled if this is NOT blank)</param>
+        /// <returns></returns>
+        public string AddBooking(int employeeID, int userID, DateTime bookingDateTime, int duration, string description, string cancellation = "")
+        {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
+            if (userID != clientUserID && clientPermissionLevel < 2) return "Insufficient Permissions";
+            string bookingTime = bookingDateTime.ToString("yyyMMddHHmm");
+
+            List<string> newData = new List<string>() { employeeID.ToString(), userID.ToString(), bookingTime, duration.ToString(), description, cancellation };
+            List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
+            return ServerCommunication.NewTableEntry("Employees", encryptedData);   //Add data to table
+        }
+        /// <summary>
+        /// Creates a new mail entry in the database
+        /// </summary>
+        /// <param name="userID">ID of user sending mail</param>
+        /// <param name="employeeID">ID of employee recipient</param>
+        /// <param name="message">Message string</param>
+        /// <returns></returns>
+        public string AddMail(int userID, int employeeID, string message)
+        {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
+            if (userID != clientUserID) return "Invalid request (you cannot send mail on behalf of another user)";
+            string sentTime = DateTime.Now.ToString("yyyMMddHHmm");
+
+            List<string> newData = new List<string>() { userID.ToString(), employeeID.ToString(), message, sentTime };
+            List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
+            return ServerCommunication.NewTableEntry("Employees", encryptedData);   //Add data to table
+        }
+        /// <summary>
+        /// Create a new invite and add to database
+        /// </summary>
+        /// <param name="businessID">Business ID for sending invite</param>
+        /// <param name="userID">Target user ('0' for any user)</param>
+        /// <param name="Expirary">DateTime for expirary(Irrelivant if noExpirary == true)</param>
+        /// <param name="Uses">Number of uses allowed ('-1' for unlimited)</param>
+        /// <param name="noExpirary">True if invite does not expire</param>
+        /// <returns></returns>
+        public string AddInvite(int businessID, int userID, DateTime Expirary, int Uses, bool noExpirary = false)
+        {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
+            if (clientBusinessID != businessID || clientPermissionLevel < 2) return "Insufficient Permissions";
+
+            string expiraryTime = Expirary.ToString("yyyMMddHHmm");
+            if (noExpirary) expiraryTime = "NONE";
+
+            string inviteTime = DateTime.Now.ToString("yyyMMddHHmm"); //Current time of sending
+            string inviteCode = GenerateInviteCode();
+
+            List<string> newData = new List<string>() { businessID.ToString(), userID.ToString(), inviteCode, inviteTime, expiraryTime, Uses.ToString() };
+            List<string> encryptedData = EncryptList(newData);                      //Encrypt all data
+            return ServerCommunication.NewTableEntry("Employees", encryptedData);   //Add data to table
+        }
         #endregion
         #region General Fetch||Edit||Delete Commands
         /// <summary>
@@ -415,6 +513,7 @@ namespace BookItDependancies
         /// <returns></returns>
         public List<string> FetchData(string tableName, List<string> columnsRequested, int queryID)
         {
+            if (!ServerCommunication.IsActive) return new List<string>() { "Connection not set" }; //Ensure server communication is active
             if (!SecurityManager.ValidateFetchRequest(tableName, columnsRequested, queryID, clientUserID, clientPermissionLevel, clientBusinessID, clientEmployeeID, clientEmployeePermissionLvl))
                 return new List<string>() { "Insufficient Permissions" };
             //If user has requried permissions, then fetch data
@@ -432,14 +531,28 @@ namespace BookItDependancies
         /// <returns></returns>
         public string EditData(string tableName, List<string> columnsRequested, int queryID, List<string> updatedData)
         {
+            if (!ServerCommunication.IsActive) return "Connection not set"; //Ensure server communication is active
             if (!SecurityManager.ValidateEditRequest(tableName, columnsRequested, queryID, clientUserID, clientPermissionLevel, clientBusinessID, clientEmployeeID, clientEmployeePermissionLvl))
                 return "Insufficient Permissions";
             //If user has requried permissions, then edit data
             List<string> encryptData = EncryptList(updatedData);
             return ServerCommunication.GeneralUpdateNonQuery(tableName, queryID.ToString(), columnsRequested, encryptData);
         }
+
+        /// <summary>
+        /// Removes user from the database, must have admin privallages
+        /// </summary>
+        /// <param name="queryID">ID of row to be deleted</param>
+        /// <returns></returns>
+        public string DeleteTableRow(string tableName, int queryID)
+        {
+            //Ensure that server communication is active (i.e. user is logged in) AND User has required permissions to delete a row
+            if (!ServerCommunication.IsActive || clientPermissionLevel < 3 || !ValidateTableName(tableName))
+                return null;
+            return ServerCommunication.DeleteRow(tableName, queryID.ToString());
+        }
         #endregion
-        #endregion
+
         #region Internal routines
         /// <summary>
         /// Converts bool array to int array (Used for column numbering)
@@ -513,6 +626,22 @@ namespace BookItDependancies
                 Encrypted.Add(SecurityManager.EncryptSK(s));
             }
             return Encrypted;
+        }
+        #endregion
+
+        #region DataHandling routines
+        private string FormatAvailability(List<string> availability) { return null; }
+        private string FormatAmmendments(List<string> ammendments) { return null; }
+
+        private List<string> GetAvailability(string availability) { return null; }
+        private List<string> GetAmmendments(string ammendments) { return null; }
+
+        private string GenerateInviteCode()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHJKLMNOPQRSTUVWXYabcdefghijkmnopqrstuvwxy0123456789"; //Note the following characters have been omitted : 'I' 'Z' 'l' 'z' (for convienience)
+            return new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
         #endregion
     }
